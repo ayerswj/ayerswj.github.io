150c53f522ff70faf9c1d765682ce31381daf5c6
ayerswj <ayerswjackson@gmail.com>
2022-06-27 21:59:32 -0500

    update cards

--- assets/js/experience.js
+++ assets/js/experience.js
@@ -5,26 +5,33 @@ AOS.init();
 const experiencecards = document.querySelector(".experience-cards");
 const exp = [
   {
-    title: "Software Development Intern",
-    cardImage: "assets/images/experience-page/flipkart.jpg",
-    place: "Flipkart",
-    time: "(May, 2020 - present)",
-    desp: "<li>Worked to migrate Flipkart lite’s product page to AMP pages so that they load up instantly.</li> <li>Made changes in the current progressive web app of Flipkart to react to different actions performed on AMP page.</li> <li>Created a node module called “fk-amp” which abstracts all the files and middlewares and can be easily imported and used from express server.</li>",
+    title: "Software Engineer",
+    cardImage: "assets/images/experience-page/USAA-Symbol.jpg",
+    place: "USAA",
+    time: "(Sep, 2020 - present)",
+    desp: "<li>Digital Personalization Team</li><li>Made changes in the current progressive web app of Flipkart to react to different actions performed on AMP page.</li> <li>Created a node module called “fk-amp” which abstracts all the files and middlewares and can be easily imported and used from express server.</li>",
   },
   {
-    title: "Student Developer",
-    cardImage: "assets/images/experience-page/gsoc.png",
-    place: "Google Summer Of Code",
-    time: "(Mar - Aug, 2019)",
+    title: "Software Engineer Intern",
+    cardImage: "assets/images/experience-page/CDK-Global.png",
+    place: "CDK Global",
+    time: "(Jun - Aug, 2019)",
     desp: "<li>Worked with MOZILLA as a part of Google Summer Of Code.</li><li>Worked on Extension Activity Monitor which is a privileged extension for Firefox that uses the activityLog API (privileged API) to monitor the activities of the other installed extensions.</li><li>The activityLog API listens for logs from other installed extensions.</li>",
   },
   {
-    title: "Research Intern",
-    cardImage: "assets/images/experience-page/IIT_Bombay.jpg",
-    place: "IIT, Bombay",
-    time: "(Dec, 19 - Jan, 20)",
+    title: "Technical Support Representative",
+    cardImage: "assets/images/experience-page/Gym-Launch.jpg",
+    place: "Gym Launch Secrets",
+    time: "(Feb - Aug, 18)",
     desp: "<li>Worked on the project “LTI (Learning Tools Interoperability) 2.0 standards Implementation for ekShiksha.</li><li>Designed a software that would help faculty to create a quiz using the questions from the database based on his/her choice of topics and he should be provided with various facilities and options to create a quiz of his choice.</li>",
   },
+  {
+    title: "Customer Service Representative",
+    cardImage: "assets/images/experience-page/FGT.jpg",
+    place: "Front Gate Tickets",
+    time: "(Oct, 17 - Feb, 18)",
+    desp: "<li>Worked on the project “LTI (Learning Tools Interoperability) 2.0 standards Implementation for ekShiksha.</li><li>Designed a software that would help faculty to create a quiz using the questions from the database based on his/her choice of topics and he should be provided with various facilities and options to create a quiz of his choice.</li>",
+  }
 ];
 
 const showCards2 = () => {
